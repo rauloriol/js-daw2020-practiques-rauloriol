e9cb9b69b5fc6d56b75ccb40f61dc8f05ce5a9db
rauloriol <eloriol@gmail.com>
2020-11-28 10:23:29 +0100

    UD10 Gestion de Eventos

--- Practicas/app/scripts/Practica1_Gestion_eventos.js
+++ Practicas/app/scripts/Practica1_Gestion_eventos.js
@@ -1,7 +1,7 @@
 // Al presionar Alt+F12 cambia el fondo de pantalla que viene de un url dado
-document.addEventListener('keydown', function (evento) {
-  if (evento.altKey && evento.key == 'F12') {
-    document.body.style.backgroundImage =
-      'url(https://source.unsplash.com/random)';
+document.addEventListener('keyup', function (evento) {
+  if (evento.altKey && evento.key === 'F12') {
+    const img = document.getElementById('imagen');
+    img.style.backgroundImage = 'url(https://source.unsplash.com/random)';
   }
 });
