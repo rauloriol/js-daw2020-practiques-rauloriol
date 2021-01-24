d084aff2cfeb7490dabd3a8ca32e5555e136bf5e
rauloriol <eloriol@gmail.com>
2021-01-24 22:13:32 +0100

    Jquery

--- Practicas/app/scripts/Jquery_practica2/index.js
+++ Practicas/app/scripts/Jquery_practica2/index.js
@@ -5,7 +5,14 @@ $(() => {
   // Primera letra de li en mayuscula
   $('div#container > ul > li').addClass('big-letter');
 
+  // Añadir logos a los enlaces
   $('a[href^="mailto"]').addClass('mailto');
   $('a[href$=".pdf"]').addClass('pdflink');
   $('a[href^="http"],a[href~="henry"]').addClass('external');
+
+  // Cambiar color de  las filas de una tabla que son impares con odd/even
+  $('table tr:nth-child(odd)').addClass('alt');
+
+  // Marcar en negrita las obras referidas a Henry
+  $('table tr  td^="Henry"').addClass('highlight');
 });
