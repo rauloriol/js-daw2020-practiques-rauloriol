d59fafc1c8290ff4d60357b153a010ca82805630
rauloriol <eloriol@gmail.com>
2021-01-29 10:03:17 +0100

    Jquery

--- Practicas/app/scripts/Jquery_practica4/p4.js
+++ Practicas/app/scripts/Jquery_practica4/p4.js
@@ -3,30 +3,28 @@
  * Crear links para back to the top
  */
 
-// creacion de un p donde se dirige el href
-/* $('body').find('h1').before().append('<p id="inicio"></p>'); */
-// despues de cada p inserta el enlace
-/* $('div.chapter').find('p').after('<a href="#inicio">Back to the top</a>'); */
+/* var $link = $('<a href="#container">Back to the top</a>'); */
+/* $('div.chapter').find('p').after($link); */
 
 /**
  * PUNTO2
  * Mover los foodnoter
  */
-const $footer = $('.footnote').get(); // seleccionar los spans
-$('div#footer').before($footer); // Seleccionar el footer y instroducir los spans antes
+// const $footer = $('.footnote').get(); // seleccionar los spans
+/// $('div#footer').before($footer); // Seleccionar el footer y instroducir los spans antes
 
 /**
  * PUNTO3
  * Introducir las notas en una lista numerada
  */
-$('div#footer').before('<ol id="listanumerada"></ol>'); // crear una lista ordenda antes del footer
+/* $('div#footer').before('<ol id="listanumerada"></ol>'); // crear una lista ordenda antes del footer
 const $lista2 = $('ol#listanumerada'); // seleccionar la lista
 const $footer2 = $('.footnote').get(); // seleccionar los spans
 
 // Recorrer los spans y añadirlos dentro de la lista ordendada
 for (const elemento of $footer2) {
   $lista2.append($('<li>').append(elemento));
-}
+} */
 /* ================================================================================= */
 
 /**
@@ -34,10 +32,16 @@ for (const elemento of $footer2) {
  * Mover los foodnoter y introducirlos en una lista ordenada
  */
 
+$('div.chapter')
+  .after($('span.footnote'))
+  .nextAll(':not(#footer)')
+  .wrapAll('<ol></ol>')
+  .wrapInner('<li></li>');
+
 /**
  * PUNTO 5
  * Crear links para back to the top
  */
 
-$('<p id="inicio"></p>').prependTo('body h1'); // insertar un parrafo antes de body h1
-$('<a href="#inicio">Back to the top</a>').insertAfter('div.chapter p'); // Despues de cada p entro del div de clase chapter introducir el contenido
+const $link = $('<a href="#container">Back to the top</a>');
+$link.insertAfter('div.chapter  p'); // Despues de cada p entro del div de clase chapter introducir el contenido
