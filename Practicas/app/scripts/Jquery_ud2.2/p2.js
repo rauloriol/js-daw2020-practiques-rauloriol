c4dc0d78299c747860992751aedb554400bfbc10
rauloriol <eloriol@gmail.com>
2021-02-01 10:12:50 +0100

    Jquery2

--- Practicas/app/scripts/Jquery_ud2.2/p2.js
+++ Practicas/app/scripts/Jquery_ud2.2/p2.js
@@ -1,17 +1,18 @@
-// cuando carge la pagina borrar los botones de dentro
-// pasar raton por encima cambiar a verde
-
 const $switcher = $('div.switcher');
 const $boton = $('<div/>', {
   html:
-    '<button id="default">Defecto</button><button id="narrow">Estret</button><button id="large">Gran</button>',
-  class: 'cambiarEstilos',
-}); /* Falta añadir hidden en class */
+    '<button class="boton">Defecto</button><button class="boton">Estrecho</button><button class="boton">Grande</button>',
+  class: 'cambiarEstilos hidden',
+});
 
 $(document).ready(() => {
   // cuando carge la pagina borrar los botones de dentro
   $switcher.children('button').remove();
   $switcher.append($boton); // añadimos los nuevos botones
+
+  const $botonesEstilos = $switcher.children('div.cambiarEstilos');
+  const $defecto = $switcher.find('div button').eq(0);
+  $defecto.addClass('selected');
   const $estrecho = $switcher.find('div button').eq(1);
   const $ancho = $switcher.find('div button').eq(2);
 
@@ -19,17 +20,32 @@ $(document).ready(() => {
     $(this).toggleClass('hover'); // cuando pasa el raton por encima, se pone y se quita
   });
 
-  /* $switcher.on('click', function (ev) {
-    $(this).children('div.cambiarEstilos').toggleClass('hidden');
-    poner hidden en cambiarestlos
-  }); */
+  $switcher.on('click', function (ev) {
+    $botonesEstilos.toggleClass('hidden');
+  });
 
+  $defecto.on('click', function (ev) {
+    $botonesEstilos.toggleClass('hidden');
+    $estrecho.removeClass('selected');
+    $ancho.removeClass('selected');
+    $(this).addClass('selected');
+    $('main').removeClass('poem chapter narrow large');
+  });
   $estrecho.on('click', function (ev) {
-    console.log('has hecho click');
-    $('main').toggleClass('narrow chapter');
+    $botonesEstilos.toggleClass('hidden');
+    $defecto.removeClass('selected');
+    $ancho.removeClass('selected');
+    $(this).addClass('selected');
+
+    $('main').removeClass('large chapter ');
+    $('main').addClass('narrow chapter ');
   });
   $ancho.on('click', function (ev) {
-    console.log('has hecho click');
-    $('main').toggleClass('large chapter');
+    $botonesEstilos.toggleClass('hidden');
+    $defecto.removeClass('selected');
+    $estrecho.removeClass('selected');
+    $(this).addClass('selected');
+    $('main').removeClass('narrow chapter ');
+    $('main').addClass('large chapter ');
   });
 });
