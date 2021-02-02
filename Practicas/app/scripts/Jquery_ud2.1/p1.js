117164be78b3c042a57bffa1878b55b87e0e72ed
rauloriol <eloriol@gmail.com>
2021-02-02 21:02:00 +0100

    Jquery 2

--- Practicas/app/scripts/Jquery_ud2.1/p1.js
+++ Practicas/app/scripts/Jquery_ud2.1/p1.js
@@ -1,7 +1,14 @@
-$('body')
-  .children('p')
-  .on('click', function (evento) {
-    evento.preventDefault();
-    console.log('has hecho click');
+$(() => {
+  const $p = $('p');
+  const texto = $p.text().trim(); // Obtenemos el texto en el <p>
+  const palabras = texto.split(' '); // Obtenemos un arreglo de palabras
+  const spans = palabras.map((p) => `<span>${p}</span> `); // Metemos las palabras en <span>
+
+  // Metemos los <span> dentro del <p>
+  $p.html(spans);
+
+  // Suscribimos una funcion para escuchar el click sobre los <span>
+  $p.on('click', 'span', function () {
     $(this).css({ background: 'yellow', 'font-weight': 'bold' });
   });
+});
