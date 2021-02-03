452ea4db9ee3711afbc5f3ebc59252f24370234a
rauloriol <eloriol@gmail.com>
2021-02-03 17:20:40 +0100

    Jquery 2

--- Practicas/app/scripts/Jquery_ud2.1/p1.js
+++ Practicas/app/scripts/Jquery_ud2.1/p1.js
@@ -1,14 +1,14 @@
 $(() => {
-  const $p = $('p');
-  const texto = $p.text().trim(); // Obtenemos el texto en el <p>
-  const palabras = texto.split(' '); // Obtenemos un arreglo de palabras
-  const spans = palabras.map((p) => `<span>${p}</span> `); // Metemos las palabras en <span>
+  const $parrafo = $('p');
+  const contenido = $parrafo.text().trim(); // Obtenemos el contenido del <p> parrafo y eliminamos los espacios
+  const palabras = contenido.split(' '); // Lo guardo en un array
 
-  // Metemos los <span> dentro del <p>
-  $p.html(spans);
+  const spanPalabras = palabras.map((p) => `<span>${p}</span> `); // Metemos las palabras en <span> con la funcion map creando un nuevo array de palabras
 
-  // Suscribimos una funcion para escuchar el click sobre los <span>
-  $p.on('click', 'span', function () {
+  $parrafo.html(spanPalabras); // Introducir los spanPalabras dentro del parrafo con html()
+
+  // Crear un evento para que cambie las propiedades de color y tipo de fuente al hacer click a los nuevos Spans
+  $parrafo.on('click', 'span', function (ev) {
     $(this).css({ background: 'yellow', 'font-weight': 'bold' });
   });
 });
--- Practicas/app/scripts/Jquery_ud2.2/p2.js
+++ Practicas/app/scripts/Jquery_ud2.2/p2.js
@@ -1,29 +1,43 @@
 const $switcher = $('div.switcher');
+
+// Creacion de los botones
 const $boton = $('<div/>', {
   html:
-    '<button data-style="default">Defecto</button><button data-style="narrow">Estrecho</button><button data-style="large">Grande</button>',
+    '<button data-style="default">Defecto</button>' +
+    '<button data-style="narrow">Estrecho</button>' +
+    '<button data-style="large">Grande</button>',
   class: 'cambiarEstilos hidden',
 });
 
 $(document).ready(() => {
-  // cuando carge la pagina borrar los botones de dentro
-  $switcher.children('button').remove();
+  $switcher.children('button').remove(); // cuando carge la pagina borrar los botones de dentro
   $switcher.append($boton); // añadimos los nuevos botones
 
   const $botonesEstilos = $switcher.children('div.cambiarEstilos');
+
+  // Seleccionar cada uno de los botones segun su posicion
   const $defecto = $switcher.find('div button').eq(0);
-  $defecto.addClass('selected');
+  $defecto.addClass('selected'); // por defecto el primer boton tiene la clase (Selected)
   const $estrecho = $switcher.find('div button').eq(1);
   const $ancho = $switcher.find('div button').eq(2);
 
+  // Cuando pasa el raton por encima, se pone y se quita la clase hover
   $switcher.hover(function (ev) {
-    $(this).toggleClass('hover'); // cuando pasa el raton por encima, se pone y se quita
+    $(this).toggleClass('hover');
   });
 
+  // Al hacer click se activa o desactiva la clase hidden
   $switcher.on('click', function (ev) {
     $botonesEstilos.toggleClass('hidden');
   });
 
+  /*************************************************
+   *
+   *        Funcionalidades de los botones
+   *
+   **************************************************/
+
+  // Elimina los estilos aplicados, elimina la clase selected de los demás
   $defecto.on('click', function (ev) {
     $botonesEstilos.toggleClass('hidden');
     $estrecho.removeClass('selected');
@@ -31,6 +45,8 @@ $(document).ready(() => {
     $(this).addClass('selected');
     $('main').removeClass();
   });
+
+  // Aplica el estilo estrecho, elimina la clase selected de los demás
   $estrecho.on('click', function (ev) {
     $botonesEstilos.toggleClass('hidden');
     $defecto.removeClass('selected');
@@ -38,6 +54,8 @@ $(document).ready(() => {
     $(this).addClass('selected');
     $('main').removeClass().addClass('narrow chapter ');
   });
+
+  // Aplica el estilo ancho, elimina la clase selected de los demás
   $ancho.on('click', function (ev) {
     $botonesEstilos.toggleClass('hidden');
     $defecto.removeClass('selected');
